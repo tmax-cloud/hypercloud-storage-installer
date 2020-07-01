fe9c2c42d478584f01b7be608421b31f74dfeab9
hyeongbin_kim <hyeongbin_kim@tmax.co.kr>
2020-07-01 16:15:03 +0900

    Update hcsctl binary
    - sample yamls are already included in hcsctl binary

--- script.sh
+++ script.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-if  [ "$#" -ne 1 ]; then
-    echo "usage : $0 {registry endpoint}"
-    echo "example : $0 192.168.50.207:5000"
+if  [ "$#" -ne 2 ]; then
+    echo "usage : $0 {registry endpoint} {inventory name}"
+    echo "example : $0 192.168.50.207:5000 test"
     exit 0
 fi
 
 registry_endpoint=$1
+inventory_name=$2
 
-sed -i 's/{registry_endpoint}/'$registry_endpoint'/g' cdi/*.yaml
-sed -i 's/{registry_endpoint}/'$registry_endpoint'/g' rook/*.yaml
+sed -i 's/{registry_endpoint}/'$registry_endpoint'/g' $inventory_name/cdi/*.yaml
+sed -i 's/{registry_endpoint}/'$registry_endpoint'/g' $inventory_name/rook/*.yaml
 
